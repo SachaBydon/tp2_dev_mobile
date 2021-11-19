cae837e3397b9f9aed1ab03224d1395ca11a8a66
SachaBydon <42966540+SachaBydon@users.noreply.github.com>
2021-11-19 16:08:15 +0100

    Add email and password editon

--- lib/screens/profil.dart
+++ lib/screens/profil.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tp2_dev_mobile/models/app_state.dart';
-
-import 'package:tp2_dev_mobile/screens/basket.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
+
+import 'package:tp2_dev_mobile/models/app_state.dart';
+import 'package:tp2_dev_mobile/utils.dart';
+import 'package:tp2_dev_mobile/widgets/topbar.dart';
 
 class Profil extends StatefulWidget {
   const Profil({Key? key}) : super(key: key);
@@ -17,103 +18,102 @@ class Profil extends StatefulWidget {
 class _ProfilState extends State<Profil> {
   final AppState appState = GetIt.instance.get<AppState>();
 
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const TopBar('Profile'),
-            Expanded(child: SingleChildScrollView(child: form()))
-          ],
-        ));
+        body: SizedBox(
+            height: double.infinity,
+            child: Stack(children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const TopBar('Profile'),
+                  Expanded(child: SingleChildScrollView(child: form()))
+                ],
+              ),
+              if (isLoading)
+                Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+            ])));
   }
+
+  TextEditingController intialdateval = TextEditingController(text: '');
 
   Widget form() {
     return FutureBuilder(
       future: getDefaultData(),
       builder: (context, AsyncSnapshot<UserData> snapshot) {
         UserData? userData = snapshot.data;
+        if (userData?.birth != '' && userData?.birth != null) {
+          intialdateval.text =
+              formatDate(DateTime.parse(userData?.birth ?? ''));
+        }
+
         if (snapshot.hasData && userData != null) {
           return Container(
               padding: const EdgeInsets.all(20),
               child: Wrap(runSpacing: 10, children: [
                 TextFormField(
-                    initialValue: userData.email,
-                    keyboardType: TextInputType.emailAddress,
-                    enabled: false,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      labelText: 'Email',
-                    )),
+                  initialValue: userData.email,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: textFormFieldDecoration('Email', context),
+                  onChanged: (value) {
+                    userData.email = value.toString();
+                  },
+                ),
                 TextFormField(
                   initialValue: userData.password,
                   obscureText: true,
-                  enabled: false,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    labelText: 'Mot de passe',
-                  ),
+                  decoration: textFormFieldDecoration('Mot de passe', context),
                   onChanged: (value) {
                     userData.password = value.toString();
                   },
                 ),
-                Container(
-                  padding: const EdgeInsets.only(left: 12, right: 12),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.grey)),
-                  child: Theme(
-                    data: ThemeData(
-                        colorScheme:
-                            const ColorScheme.light(primary: Color(0xff26AE60)),
-                        inputDecorationTheme: const InputDecorationTheme(
-                          border: InputBorder.none,
-                        )),
-                    child: InputDatePickerFormField(
-                      fieldHintText: 'Date de naissance',
-                      fieldLabelText: 'Date de naissance',
-                      initialDate: DateTime.parse(userData.birth),
-                      firstDate: DateTime(1970),
-                      lastDate: DateTime.now(),
-                      onDateSaved: (date) {
-                        userData.birth = date.toString();
-                      },
-                    ),
-                  ),
+                TextFormField(
+                  readOnly: true,
+                  autocorrect: false,
+                  maxLines: 1,
+                  controller: intialdateval,
+                  decoration:
+                      textFormFieldDecoration('Date de naissance', context),
+                  onTap: () async {
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                    DateTime? date = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime.now());
+
+                    if (date != null) {
+                      userData.birth = date.toString();
+                      intialdateval.text = formatDate(date);
+                    }
+                  },
                 ),
                 TextFormField(
                   initialValue: userData.address,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    labelText: 'Adresse',
-                  ),
+                  decoration: textFormFieldDecoration('Adresse', context),
                   onChanged: (value) {
                     userData.address = value.toString();
                   },
                 ),
                 TextFormField(
                   initialValue: userData.postcode,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    labelText: 'Code postale',
-                  ),
+                  decoration: textFormFieldDecoration('Code postale', context),
                   onChanged: (value) {
                     userData.postcode = value.toString();
                   },
                 ),
                 TextFormField(
                   initialValue: userData.city,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    labelText: 'Ville',
-                  ),
+                  decoration: textFormFieldDecoration('Ville', context),
                   onChanged: (value) {
                     userData.city = value.toString();
                   },
@@ -140,6 +140,7 @@ class _ProfilState extends State<Profil> {
                         ],
                       ),
                       style: ElevatedButton.styleFrom(
+                        primary: Color(0xff26AE60),
                         shadowColor: Colors.transparent,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
@@ -207,28 +208,36 @@ class _ProfilState extends State<Profil> {
     FocusScope.of(context).unfocus();
 
     String userUID = appState.user?.uid ?? '';
+    setState(() => isLoading = true);
 
     await FirebaseFirestore.instance.collection('users').doc(userUID).update({
       'address': userData.address,
       'birth': userData.birth,
       'city': userData.city,
       'postcode': userData.postcode,
-    }).then((_) {
-      print('res: profile updated');
-    }).catchError((error) {
-      print('res: error: $error');
     });
+
+    if (userData.email != userData.previousEmail) {
+      // Update email
+      await appState.user?.updateEmail(userData.email);
+    }
+
+    if (userData.password != userData.previousPassword) {
+      // Update password
+      await appState.user?.updatePassword(userData.password);
+    }
+
+    setState(() => isLoading = false);
   }
 
   Future<UserData> getDefaultData() async {
-    var userUID = appState.user?.uid ?? '';
-    var userEmail = appState.user?.email ?? '';
-    var userFakePassword = '............';
+    String userUID = appState.user?.uid ?? '';
+    String userEmail = appState.user?.email ?? '';
 
     DocumentSnapshot<Map<String, dynamic>> query =
         await FirebaseFirestore.instance.collection('users').doc(userUID).get();
 
-    var defaultUserData = UserData(userEmail, userFakePassword, query['birth'],
+    var defaultUserData = UserData(userEmail, userEmail, '', '', query['birth'],
         query['address'], query['postcode'], query['city']);
 
     return defaultUserData;
@@ -237,12 +246,35 @@ class _ProfilState extends State<Profil> {
 
 class UserData {
   String email;
+  String previousEmail;
   String password;
+  String previousPassword;
+
   String birth;
   String address;
   String postcode;
   String city;
 
-  UserData(this.email, this.password, this.birth, this.address, this.postcode,
-      this.city);
+  UserData(this.email, this.previousEmail, this.password, this.previousPassword,
+      this.birth, this.address, this.postcode, this.city);
 }
+
+formatDate(DateTime date) {
+  String day = (date.day < 10) ? '0${date.day}' : date.day.toString();
+  String month = (date.month < 10) ? '0${date.month}' : date.month.toString();
+  String year = date.year.toString();
+  return '$day/$month/$year';
+}
+
+//Firebase Auth class to handle user authentication
+// class Auth {
+//   final FirebaseAuth auth = FirebaseAuth.instance;
+
+//   Future<void> updateEmail(User user, String new_email) async {
+//     await user.updateEmail(new_email);
+//   }
+
+//   Future<void> updatePassword(User user, String new_password) async {
+//     await user.updatePassword(new_password);
+//   }
+// }
