0e4958b4d081052758e707c391b18cc1acd6a9d0
SachaBydon <42966540+SachaBydon@users.noreply.github.com>
2021-11-19 16:07:41 +0100

    Add signin screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:tp2_dev_mobile/models/auth.dart';
+import 'package:tp2_dev_mobile/widgets/logo.dart';
 import 'package:tp2_dev_mobile/models/app_state.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatefulWidget {
-  const Login({
-    Key? key,
-    /*required this.login*/
-  }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
   _LoginState createState() => _LoginState();
@@ -23,156 +22,30 @@ class _LoginState extends State<Login> {
   bool showPassword = false;
 
   //Firebase authentication instance
-  var authHandler = Auth();
+  AuthActions authHandler = AuthActions();
+
+  bool isLoading = false;
 
   /// Method to check if the form is valid and if it is log the user in
   submitForm() async {
     FocusScope.of(context).unfocus();
-    if (_formKey.currentState!.validate()) {
-      checkAuth(_loginValue, _passwordValue, context, authHandler);
-    }
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey.shade300,
-        body: SizedBox(
-            height: double.infinity,
-            child: Wrap(runAlignment: WrapAlignment.end, children: [
-              Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(40),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Connexion',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 20),
-                      Form(
-                        key: _formKey,
-                        child: Wrap(
-                            runSpacing: 16,
-                            runAlignment: WrapAlignment.center,
-                            children: <Widget>[
-                              TextFormField(
-                                keyboardType: TextInputType.emailAddress,
-                                validator: (value) {
-                                  if (value == '') {
-                                    return 'Entrez une adresse mail valide';
-                                  } else {
-                                    _loginValue = value.toString();
-                                    return null;
-                                  }
-                                },
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                  labelText: 'Email',
-                                ),
-                              ),
-                              TextFormField(
-                                keyboardType: TextInputType.visiblePassword,
-                                validator: (value) {
-                                  if (value == '') {
-                                    return 'Entrez votre mot de passe';
-                                  } else {
-                                    _passwordValue = value.toString();
-                                    return null;
-                                  }
-                                },
-                                obscureText: !showPassword,
-                                decoration: InputDecoration(
-                                  suffixIcon: IconButton(
-                                      icon: showPassword
-                                          ? const Icon(Icons.visibility_off)
-                                          : const Icon(Icons.visibility),
-                                      onPressed: () => {
-                                            setState(() {
-                                              showPassword = !showPassword;
-                                            })
-                                          }),
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                  labelText: 'Mot de passe',
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () => submitForm(),
-                                child: Wrap(
-                                  runAlignment: WrapAlignment.center,
-                                  crossAxisAlignment: WrapCrossAlignment.center,
-                                  spacing: 10,
-                                  children: const [
-                                    Icon(Icons.login),
-                                    Text(
-                                      'Se connecter',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  shadowColor: Colors.transparent,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  padding: const EdgeInsets.only(
-                                      bottom: 18, top: 18),
-                                  minimumSize: const Size(double.infinity, 30),
-                                ),
-                              ),
-                            ]),
-                      ),
-                      const SizedBox(height: 15),
-                      InkWell(
-                        onTap: () => {print('mdp oublié')},
-                        child: Text(
-                          'Mot de passe oublié ?',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(height: 50),
-                      Center(
-                          child: InkWell(
-                        onTap: () => {print('créer un compte')},
-                        child: Column(
-                          children: [
-                            Text('Vous n\'avez pas de compte ?',
-                                style: TextStyle(color: Colors.grey.shade600)),
-                            Text('Créer un compte',
-                                style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                    fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ))
-                    ],
-                  ))
-            ])));
-  }
+    bool isValid = _formKey.currentState!.validate();
 
-  Future<void> checkAuth(
-      loginValue, passwordValue, context, authHandler) async {
-    authHandler.handleSignInEmail(loginValue, passwordValue).then((user) {
+    if (!isValid) return;
+
+    setState(() => isLoading = true);
+
+    try {
+      User user =
+          await authHandler.handleSignInEmail(_loginValue, _passwordValue);
+
       //Authentication successful
       AppState appState = GetIt.instance.get<AppState>();
       appState.login(user);
-      saveAuthData(loginValue, passwordValue);
+      authHandler.saveAuthData(_loginValue, _passwordValue);
       Navigator.pushReplacementNamed(context, '/home');
-    }).catchError((e) {
+    } catch (e) {
       //Authentication failed
       String errorMessage = e.toString();
       if (errorMessage.split('] ').length > 1) {
@@ -185,29 +58,152 @@ class _LoginState extends State<Login> {
             behavior: SnackBarBehavior.floating,
             backgroundColor: Colors.red),
       );
-    });
-  }
-
-  saveAuthData(login, password) async {
-    try {
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setString('user_login', login);
-      prefs.setString('user_password', password);
-    } catch (e) {
-      print(e);
     }
+
+    setState(() => isLoading = false);
   }
-}
 
-//Firebase Auth class to handle user authentication
-class Auth {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-
-  Future<User> handleSignInEmail(String email, String password) async {
-    UserCredential result =
-        await auth.signInWithEmailAndPassword(email: email, password: password);
-    final User user = result.user!;
-
-    return user;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey.shade300,
+        body: Container(
+            margin: const EdgeInsets.only(top: 25),
+            child: Stack(children: [
+              Column(children: [
+                if (MediaQuery.of(context).size.height > 300)
+                  Expanded(child: Logo()),
+                Container(
+                    constraints: BoxConstraints(
+                      maxHeight: MediaQuery.of(context).size.height -
+                          MediaQuery.of(context).viewInsets.bottom -
+                          25,
+                    ),
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(40),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: SingleChildScrollView(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Connexion',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 20),
+                        Form(
+                          key: _formKey,
+                          child: Wrap(
+                              runSpacing: 16,
+                              runAlignment: WrapAlignment.center,
+                              children: <Widget>[
+                                TextFormField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  validator: (value) {
+                                    if (value == '') {
+                                      return 'Entrez une adresse mail valide';
+                                    } else {
+                                      _loginValue = value.toString();
+                                      return null;
+                                    }
+                                  },
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    labelText: 'Email',
+                                  ),
+                                ),
+                                TextFormField(
+                                  keyboardType: TextInputType.visiblePassword,
+                                  validator: (value) {
+                                    if (value == '') {
+                                      return 'Entrez votre mot de passe';
+                                    } else {
+                                      _passwordValue = value.toString();
+                                      return null;
+                                    }
+                                  },
+                                  obscureText: !showPassword,
+                                  decoration: InputDecoration(
+                                    suffixIcon: IconButton(
+                                        icon: showPassword
+                                            ? const Icon(Icons.visibility_off)
+                                            : const Icon(Icons.visibility),
+                                        onPressed: () => {
+                                              setState(() {
+                                                showPassword = !showPassword;
+                                              })
+                                            }),
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    labelText: 'Mot de passe',
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => submitForm(),
+                                  child: Wrap(
+                                    runAlignment: WrapAlignment.center,
+                                    crossAxisAlignment:
+                                        WrapCrossAlignment.center,
+                                    spacing: 10,
+                                    children: const [
+                                      Icon(Icons.login),
+                                      Text(
+                                        'Se connecter',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shadowColor: Colors.transparent,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    padding: const EdgeInsets.only(
+                                        bottom: 18, top: 18),
+                                    minimumSize:
+                                        const Size(double.infinity, 30),
+                                  ),
+                                ),
+                              ]),
+                        ),
+                        const SizedBox(height: 50),
+                        Center(
+                            child: InkWell(
+                          onTap: () =>
+                              {Navigator.pushNamed(context, '/signin')},
+                          child: Column(
+                            children: [
+                              Text('Vous n\'avez pas de compte ?',
+                                  style:
+                                      TextStyle(color: Colors.grey.shade600)),
+                              Text('Créer un compte',
+                                  style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ))
+                      ],
+                    )))
+              ]),
+              if (isLoading)
+                Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                )
+            ])));
   }
 }
---
+++ lib/screens/signin.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:tp2_dev_mobile/models/auth.dart';
+import 'package:tp2_dev_mobile/widgets/logo.dart';
+import 'package:tp2_dev_mobile/models/app_state.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({Key? key}) : super(key: key);
+
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  //Form state
+  final _formKey = GlobalKey<FormState>();
+  final List<String?> textFieldsValue = [];
+  String _loginValue = '';
+  String _passwordValue = '';
+  String _confirmValue = '';
+  bool showPassword = false;
+  bool showConfirm = false;
+
+  //Firebase authentication instance
+  AuthActions authHandler = AuthActions();
+
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Container(
+        margin: const EdgeInsets.only(top: 25),
+        child: Stack(
+          children: [
+            Column(children: [
+              if (MediaQuery.of(context).size.height > 300)
+                Expanded(child: Logo()),
+              Container(
+                  constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height -
+                        MediaQuery.of(context).viewInsets.bottom -
+                        25,
+                  ),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(40),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Inscription',
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold)),
+                      const SizedBox(height: 20),
+                      Form(
+                        key: _formKey,
+                        child: Wrap(
+                            runSpacing: 16,
+                            runAlignment: WrapAlignment.center,
+                            children: <Widget>[
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                validator: (value) {
+                                  RegExp mailRegex = RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+                                  if (value == '' ||
+                                      !mailRegex.hasMatch(value ?? '')) {
+                                    return 'Cette adresse mail n\'est pas valide';
+                                  } else {
+                                    _loginValue = value.toString();
+                                    return null;
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  labelText: 'Email',
+                                ),
+                              ),
+                              TextFormField(
+                                keyboardType: TextInputType.visiblePassword,
+                                validator: (value) {
+                                  if (value == '') {
+                                    return 'Entrez votre mot de passe';
+                                  } else {
+                                    _passwordValue = value.toString();
+                                    return null;
+                                  }
+                                },
+                                obscureText: !showPassword,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                      icon: showPassword
+                                          ? const Icon(Icons.visibility_off)
+                                          : const Icon(Icons.visibility),
+                                      onPressed: () => {
+                                            setState(() {
+                                              showPassword = !showPassword;
+                                            })
+                                          }),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  labelText: 'Mot de passe',
+                                ),
+                              ),
+                              TextFormField(
+                                keyboardType: TextInputType.visiblePassword,
+                                validator: (value) {
+                                  if (value == '') {
+                                    return 'Confirmer votre mot de passe';
+                                  } else if (value != _passwordValue) {
+                                    return 'Les mots de passe ne correspondent pas';
+                                  } else {
+                                    _confirmValue = value.toString();
+                                    return null;
+                                  }
+                                },
+                                obscureText: !showConfirm,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                      icon: showConfirm
+                                          ? const Icon(Icons.visibility_off)
+                                          : const Icon(Icons.visibility),
+                                      onPressed: () => {
+                                            setState(() {
+                                              showConfirm = !showConfirm;
+                                            })
+                                          }),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20)),
+                                  labelText: 'Confirmer',
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () => submitForm(),
+                                child: Wrap(
+                                  runAlignment: WrapAlignment.center,
+                                  crossAxisAlignment: WrapCrossAlignment.center,
+                                  spacing: 10,
+                                  children: const [
+                                    Icon(Icons.login),
+                                    Text(
+                                      'S\'inscrire',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  shadowColor: Colors.transparent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.only(
+                                      bottom: 18, top: 18),
+                                  minimumSize: const Size(double.infinity, 30),
+                                ),
+                              ),
+                            ]),
+                      ),
+                      const SizedBox(height: 50),
+                      Center(
+                          child: InkWell(
+                        onTap: () => {Navigator.pushNamed(context, '/login')},
+                        child: Column(
+                          children: [
+                            Text('Vous avez déjà un compte ?',
+                                style: TextStyle(color: Colors.grey.shade600)),
+                            Text('Se connecter',
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ))
+                    ],
+                  )))
+            ]),
+            if (isLoading)
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  submitForm() async {
+    FocusScope.of(context).unfocus();
+    bool isValid = _formKey.currentState!.validate();
+
+    if (!isValid) return;
+
+    setState(() => isLoading = true);
+
+    Map<String, String> emptyUserData = {
+      'address': '',
+      'birth': '',
+      'city': '',
+      'postcode': '',
+      'photo': '',
+    };
+
+    try {
+      User user =
+          await authHandler.handleSignUpEmail(_loginValue, _passwordValue);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .set(emptyUserData);
+      await FirebaseFirestore.instance
+          .collection('paniers')
+          .doc(user.uid)
+          .set({'items': []});
+
+      AppState appState = GetIt.instance.get<AppState>();
+      appState.login(user);
+      authHandler.saveAuthData(_loginValue, _passwordValue);
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (e) {
+      //Authentication failed
+      String errorMessage = e.toString();
+      if (errorMessage.split('] ').length > 1) {
+        errorMessage = errorMessage.split('] ')[1];
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(errorMessage),
+            duration: const Duration(milliseconds: 1500),
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Colors.red),
+      );
+    }
+
+    setState(() => isLoading = false);
+  }
+}
